7e3046f09d14d23c8aed5aeb99c647e147a43103
eliesatpanelgrid <eliesatpanelgrid@gmail.com>
2026-05-30 21:30:35 +0300

    Add files via upload

---
+++ usr/script/Islamsalama-FuryBackUp.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/islam-2412/FuryBackUp/main/fury/installer.sh -O - | /bin/sh
---
+++ usr/script/Islamsalama-ServerColoring.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+wget -q "--no-check-certificate" https://raw.githubusercontent.com/islam-2412/ServerColoring/main/fury/installer.sh -O - | /bin/sh
